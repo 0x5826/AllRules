d77592125a735117e234c1dca01dac380ca468f8
dante <dante@seckv.com>
2025-02-19 02:11:57 +0000

    update

--- mikrotik_init_chnroute.sh
+++ mikrotik_init_chnroute.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-listname="chnroute_v4"
 path="./mikrotik"
+listname_v4="chnroute_v4"
+filename_v4="${listname_v4}.rsc"
 
-chnroute_list=$(curl -sSL https://raw.githubusercontent.com/mayaxcn/china-ip-list/master/chnroute.txt)
+chnroute_list_v4=$(curl -sSL https://raw.githubusercontent.com/mayaxcn/china-ip-list/master/chnroute.txt)
 
-echo "/ip firewall address-list remove [/ip firewall address-list find list=$listname]" > chnroute_v4.rsc
-echo "/ip firewall address-list" >> chnroute_v4.rsc
-#echo "add address=10.0.0.0/8 disabled=no list=$listname" >> chnroute_v4.rsc
-#echo "add address=172.16.0.0/12 disabled=no list=$listname" >> chnroute_v4.rsc
-#echo "add address=192.168.0.0/16 disabled=no list=$listname" >> chnroute_v4.rsc
+echo "/ip firewall address-list remove [/ip firewall address-list find list=$listname_v4]" > $path/$filename_v4
+echo "/ip firewall address-list" >> $path/$filename_v4
+#echo "add address=10.0.0.0/8 disabled=no list=$listname_v4" >> $path/$filename_v4
+#echo "add address=172.16.0.0/12 disabled=no list=$listname_v4" >> $path/$filename_v4
+#echo "add address=192.168.0.0/16 disabled=no list=$listname_v4" >> $path/$filename_v4
 
-for chnroute_ipv4 in $chnroute_list
+for chnroute_ipv4 in $chnroute_list_v4
 do
-    echo "add address=$chnroute_ipv4 disabled=no list=$listname" >> $path/chnroute_v4.rsc
+    echo "add address=$chnroute_ipv4 disabled=no list=$listname_v4" >> $path/$filename_v4
 done
